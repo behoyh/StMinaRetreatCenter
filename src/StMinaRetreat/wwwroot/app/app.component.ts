845c0a268981fadd31bd976d8292dfd721a54378
beshoy hanna <beshoyhanna@outlook.com>
2016-12-31 08:56:44 -0800

    paths placholder

--- src/StMinaRetreat/wwwroot/app/app.component.ts
+++ src/StMinaRetreat/wwwroot/app/app.component.ts
@@ -1,40 +1,28 @@
 ﻿import { Component } from '@angular/core';
 
-export class Hero {
-    id: number;
-    name: string;
+export class FilePath {
+    path: string;
 }
 
 
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+var PATHS: FilePath[] = [
+    { path: 'Sample Path' }
 ];
 
 @Component({
     selector: 'my-app',
     template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      <li *ngFor="let path of paths"
+        [class.selected]="path === selectedPath"
+        (click)="onSelect(path)">
+        <span class="badge">{{path.path}}</span>
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
+    <div *ngIf="selectedPath">
+      <h2>{{selectedPath.path}}</h2>
+      <div><label>file: </label>{{selectedPath.path}}</div>
     </div>
   `,
     styles: [`
@@ -88,11 +76,12 @@ const HEROES: Hero[] = [
   `]
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
-    heroes = HEROES;
-    selectedHero: Hero;
+    title = 'Directory';
+    selectedPath: FilePath;
 
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
+    paths = PATHS;
+
+    onSelect(path: FilePath): void {
+        this.selectedPath = path;
     }
 }

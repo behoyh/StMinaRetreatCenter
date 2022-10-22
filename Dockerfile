cf1c53c4d843afde91bce57ec2ba26eeb2e5e44e
behoyh <beshoyhanna@outlook.com>
2022-10-22 13:07:12 -0400

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY ./angular-firestore-k8s /usr/src/app
 RUN npm install -g @angular/cli
 RUN npm install
 RUN npm rebuild node-sass
-RUN ng build --prod
+RUN ng build
 FROM nginx
 COPY --from=node /usr/src/app/dist /usr/share/nginx/html
 COPY ./angular-firestore-k8s/nginx.conf /etc/nginx/conf.d/default.conf
--- angular-firestore-k8s/src/app/app.module.ts
+++ angular-firestore-k8s/src/app/app.module.ts
@@ -3,19 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRippleModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppRouterModule } from './app.router.module';
@@ -27,7 +14,6 @@ import { AppState } from './shared/app.state';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularEditorModule } from '@kolkov/angular-editor';
-import { HttpModule } from '@angular/http';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
 import { LodgingComponent } from './lodging/lodging.component';
@@ -38,9 +24,9 @@ import { NavComponent } from './nav/nav.component';
 import { SaintsComponent } from './saints/saints.component';
 import { NewsComponent } from './news/news.component';
 import { CollageComponent } from './collage/collage.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore } from 'firebase/firestore';
-import { provideFirestore } from '@angular/fire/firestore';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+
+import { getFirestore, provideFirestore, } from '@angular/fire/firestore';
 
 @NgModule({
     declarations: [
@@ -61,22 +47,9 @@ import { provideFirestore } from '@angular/fire/firestore';
         provideFirebaseApp(() => initializeApp(environment.firebase)),
         provideFirestore(() => getFirestore()),
         BrowserAnimationsModule,
-        MatButtonModule,
-        MatCheckboxModule,
-        MatFormFieldModule,
-        MatInputModule,
         FormsModule,
-        MatDialogModule,
-        MatSnackBarModule,
-        MatGridListModule,
-        MatToolbarModule,
-        MatCardModule,
-        MatIconModule,
         AppRouterModule,
         RouterModule.forRoot([], { relativeLinkResolution: 'legacy' }),
-        MatTabsModule,
-        MatRippleModule,
-        MatStepperModule,
         ReactiveFormsModule,
         NgxsModule.forRoot([
             AppState
@@ -86,10 +59,9 @@ import { provideFirestore } from '@angular/fire/firestore';
         NgxsLoggerPluginModule.forRoot(),
         //NgxsRouterPluginModule.forRoot(),
         AngularEditorModule,
-        HttpClientModule,
-        HttpModule
+        HttpClientModule
     ],
-    exports: [MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSnackBarModule, MatDialogModule, MatGridListModule, MatToolbarModule, MatCardModule, MatIconModule, MatTabsModule, MatRippleModule, MatStepperModule],
+    exports: [],
     providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
     bootstrap: [AppComponent]
 })
